75c2ba3a1f5a5b06f47485348239b2a4aa120da3
xuchenxx <wuxuchen98@gmail.com>
2022-08-21 03:22:39 +0800

    sumit

---
+++ src/loadgenerator/locustfile.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random,math
+from locust import HttpUser, TaskSet, between, constant
+from locust import LoadTestShape
+import  numpy as np
+
+products = [
+    '0PUK6V6EV0',
+    '1YMWWN1N4O',
+    '2ZYFJ3GM2N',
+    '66VCHSJNUP',
+    '6E92ZMYYFZ',
+    '9SIQT8TOJO',
+    'L9ECAV7KIM',
+    'LS4PSXUNUM',
+    'OLJCESPC7Z']
+
+def index(l):
+    l.client.get("/")
+
+def setCurrency(l):
+    currencies = ['EUR', 'USD', 'JPY', 'CAD']
+    l.client.post("/setCurrency",
+        {'currency_code': random.choice(currencies)})
+
+def browseProduct(l):
+    l.client.get("/product/" + random.choice(products))
+
+def viewCart(l):
+    l.client.get("/cart")
+
+def addToCart(l):
+    product = random.choice(products)
+    l.client.get("/product/" + product)
+    l.client.post("/cart", {
+        'product_id': product,
+        'quantity': random.choice([1,2,3,4,5,10])})
+
+def checkout(l):
+    addToCart(l)
+    l.client.post("/cart/checkout", {
+        'email': 'someone@example.com',
+        'street_address': '1600 Amphitheatre Parkway',
+        'zip_code': '94043',
+        'city': 'Mountain View',
+        'state': 'CA',
+        'country': 'United States',
+        'credit_card_number': '4432-8015-6152-0454',
+        'credit_card_expiration_month': '1',
+        'credit_card_expiration_year': '2039',
+        'credit_card_cvv': '672',
+    })
+
+class UserBehavior(TaskSet):
+
+    def on_start(self):
+        index(self)
+
+    tasks = {index: 1,
+        setCurrency: 2,
+        browseProduct: 10,
+        addToCart: 2,
+        viewCart: 3,
+        checkout: 1}
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = constant(0.5)
+
+class DoubleWave(LoadTestShape):
+    """
+    A shape to imitate some specific user behaviour. In this example, midday
+    and evening meal times. First peak of users appear at time_limit/3 and
+    second peak appears at 2*time_limit/3
+    Settings:
+        min_users -- minimum users
+        peak_one_users -- users in first peak
+        peak_two_users -- users in second peak
+        time_limit -- total length of test
+    """
+
+    time_limit=6000000000000
+
+    def tick(self):
+        run_time = round(self.get_run_time())
+
+        if run_time < self.time_limit:
+            user_count = 100*(np.sin(run_time/30))+100
+
+        return (round(user_count), round(user_count))
+       
